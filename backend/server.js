37e6218a84ac7bcc8548e7c598fd2a27f902de4e
Kalidass <kalidass.k021@gmail.com>
2024-08-16 19:37:15 +0530

    establish connection with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
-import express from 'express';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 const app = express();
+// dot env configuration
+dotenv.config();
 
+// mongodb connection
+const connectTODB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB is connected');
+    } catch (err) {
+        console.error(`MongoDB connection error ${err}`);
+        throw err;
+    }
+}
 
-app.listen(3000, () => {
-    console.log('Server is up and listening on port 3000');
+connectTODB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is up and listening on port ${PORT}`);
 });
