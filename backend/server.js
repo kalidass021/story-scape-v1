e3cc7a599b1d20487e9c5f5fc2abf0718f7cc369
Kalidass <kalidass.k021@gmail.com>
2024-08-16 23:23:52 +0530

    create signup api route

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
 // dot env configuration
 dotenv.config();
+
+// allow json as a input to the backend
+// body parser
+app.use(express.json());
 
 // mongodb connection
 const connectTODB = async () => {
@@ -27,3 +32,4 @@ app.listen(PORT, () => {
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
