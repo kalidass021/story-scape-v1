19b957e3a05c3b829e28cd39f761134316f7046e
Kalidass <kalidass.k021@gmail.com>
2024-08-24 20:30:30 +0530

    create signin api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,13 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    next(errorHandler(400, 'All fields are required'));
+    return next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -19,9 +20,50 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json("User created successfully");
+    res.status(201).json('User created successfully');
   } catch (err) {
     console.error(`Error while creating the user ${err}`);
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      // User not found
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      // Invalid password
+      return next(errorHandler(404, 'Invalid password'));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // we don't want to send the password
+    // remove the password from validUser and send it
+    const {password: pass, ...rest} = validUser._doc;
+    // if we use the validUser we'll get the same result
+    // everything is inside the validuser._doc so we need use that
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (err) {
+    console.error(`Error while sign in ${err}`);
     next(err);
   }
 };
