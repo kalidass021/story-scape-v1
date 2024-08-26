115a7e7ab32f13e1c4102654efb3f86e7c81bc37
Kalidass <kalidass.k021@gmail.com>
2024-08-26 22:06:05 +0530

    add redux persist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import AppFooter from './components/AppFooter';
-import appStore from './redux/appStore';
+import appStore, { persistor } from './redux/appStore';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <div>
-      {/* step2. Providing redux store to application */}
-      <Provider store={appStore}>
-        <Header />
-        <Outlet />
-        <AppFooter />
-      </Provider>
+      <PersistGate persistor={persistor}>
+        {/* step2. Providing redux store to application */}
+        <Provider store={appStore}>
+          <Header />
+          <Outlet />
+          <AppFooter />
+        </Provider>
+      </PersistGate>
     </div>
   );
 };
--- frontend/src/redux/appStore.js
+++ frontend/src/redux/appStore.js
@@ -1,12 +1,31 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+
+// redux persist
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // step1. create a store with configure store function
 const appStore = configureStore({
-    reducer: {
-        // step4: add slice reducers to the redux store
-        user: userReducer,
-    },
+  // step4: add slice reducers to the redux store
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({ serializableCheck: false });
+  },
 });
+
+export const persistor = persistStore(appStore);
 
 export default appStore;
