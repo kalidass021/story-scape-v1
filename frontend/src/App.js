ae0d63ae675efcb38f3a662f573110ac2ba5220f
Kalidass <kalidass.k021@gmail.com>
2024-08-29 22:42:15 +0530

    implement dark mode functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import AppFooter from './components/AppFooter';
 import appStore, { persistor } from './redux/appStore';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider';
 
 const App = () => {
   return (
-    <div>
+    <>
       <PersistGate persistor={persistor}>
         {/* step2. Providing redux store to application */}
         <Provider store={appStore}>
-          <Header />
-          <Outlet />
-          <AppFooter />
+          <ThemeProvider>
+              <Header />
+              <Outlet />
+              <AppFooter />
+          </ThemeProvider>
         </Provider>
       </PersistGate>
-    </div>
+    </>
   );
 };
 
